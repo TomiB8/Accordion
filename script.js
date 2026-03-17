a84c7475478c97203c1f418d7d6a57dee7c5e881
TomiB8 <tomasbesson8@gmail.com>
2026-03-17 19:43:43 -0300

    17/3 Ultimos cambios hechos

---
+++ script.js
@@ -0,0 +1,10 @@
+var pregunta = document.getElementsByClassName("pregunta-accordion");
+var respuesta = document.getElementsByClassName("respuesta-box");
+
+function verRespuesta(){
+    
+}
+
+pregunta.addEventListener("click",);
+
+
