1a64b42c95254b726ec65ef7e92d422d62bb2505
TomiB8 <tomasbesson8@gmail.com>
2026-03-18 00:08:19 -0300

    18/3 Terminado

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var pregunta = document.getElementsByClassName("pregunta-accordion");
-var respuesta = document.getElementsByClassName("respuesta-box");
+const botones = document.querySelectorAll(".pregunta-accordion");
 
-function verRespuesta(){
-    
-}
+botones.forEach(function(boton){
+    boton.addEventListener("click", function(){
+        const respuestaActual = this.nextElementSibling;
+        const respuestaAbierta = document.querySelector(".respuesta-box.abierta");
+        const botonActivo = document.querySelector(".pregunta-accordion.activa");
 
-pregunta.addEventListener("click",);
+        if(respuestaAbierta && respuestaAbierta !== respuestaActual){
+            respuestaAbierta.classList.remove("abierta");
+            botonActivo.classList.remove("activa");
+        }
 
-
+        respuestaActual.classList.toggle("abierta");
+        this.classList.toggle("activa");
+    });
+});
